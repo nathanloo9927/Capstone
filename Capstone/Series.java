e375ac93d03ca397cfd4821c8ef0a477d8f60e2f
nathanloo9927 <NGLoo@student.naperville203.org>
2016-04-29 11:45:22 -0500

    50

--- Capstone/Series.java
+++ Capstone/Series.java
@@ -19,4 +19,45 @@ public class Series extends BlackJack
     {
         return scoreKeep[currentPlayer];
     }
+    /*public String roundWinner()
+    {
+        int highest = 0;
+        int highestIndex = -1;
+        ArrayList<Integer> indexOfTies = new ArrayList<Integer>();
+        for (int i = 0; i < numplayers; i++)
+        {
+            if (handValue[i][1] > highest && handValue[i][1] <= 21)
+            {
+                highest = handValue[i][1];
+                highestIndex = i;
+                indexOfTies.clear();
+                indexOfTies.add(i);
+            } else if (handValue[i][1] == highest && handValue[i][1] <= 21)
+            {
+                indexOfTies.add(i);
+            }
+        }
+        for (int i = 0; i < numplayers; i++)
+        {
+            System.out.println(players[i] + "'s hand: " + privateHand[i]);
+        }
+        if (highestIndex >= 0)
+        {
+            if (indexOfTies.size() == 1)
+            {
+                scoreKeep[highestIndex]++;
+                return players[highestIndex] + " wins with " + handValue[highestIndex][1];
+            } else
+            {
+                String people = "We have a tie with " + highest + "\n";
+                for (Integer i : indexOfTies)
+                {
+                    scoreKeep[i]++;
+                    people += (players[i] + "\n");
+                }
+                return people;
+            }
+        }
+        return "No Winner. Everyone busted";
+    }*/
 }
